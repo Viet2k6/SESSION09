08cba22fe3d05ce577ffa9faf646508465d1a77a
Đăng Việt <trandangviet00@gmail.com>
2024-12-01 23:37:39 +0700

    Add files via upload

--- ss9-b1.c
+++ ss9-b1.c
@@ -3,35 +3,36 @@ int main() {
     int arr[100];           
     int currentLength = 0;  
     int n, value, addIndex;
-    printf("nhap so luong phan tu cua mang (<=100 va >0): ");
+    printf("Nhap so luong phan tu cua mang (<=100 va >0): ");
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        printf("nhap phan tu thu %d: ", i + 1);
+	for (int i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
     currentLength = n;  
-    printf("\nnhap gia tri phan tu can them: ");
+    printf("\nNhap gia tri phan tu can them: ");
     scanf("%d", &value);
-    printf("nhap vi tri can them (1 den %d): ", currentLength);
-    scanf("%d", &addIndex);
-    if (addIndex <= 0 || addIndex > currentLength) {
-        printf("vi tri khong hop le\n");
+    do {
+        printf("Nhap vi tri can them (1 den %d): ", currentLength);
+        scanf("%d", &addIndex);
+        if (addIndex <= 0 || addIndex > currentLength + 1) {
+            printf("Vi tri khong hop le. Vui long nhap lai.\n");
+        }
+    } while (addIndex <= 0 || addIndex > currentLength + 1);
+
+    if (addIndex == currentLength + 1) {
+        arr[currentLength] = value;
     } else {
-        if (addIndex == currentLength) {
-            arr[currentLength] = value;
-            currentLength++;  
-        } else {
-            for (int i = currentLength; i > addIndex; i--) {
-                arr[i] = arr[i - 1];
-            }
-            arr[addIndex-1] = value;  
-            currentLength++;  
+        for (int i = currentLength; i >= addIndex; i--) {
+            arr[i] = arr[i - 1];
         }
-        printf("\nmang sau khi them phan tu la: ");
-        for (int i = 0; i < currentLength; i++) {
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
+        arr[addIndex - 1] = value;  
     }
+    currentLength++;  
+    printf("\nMang sau khi them phan tu la: ");
+    for (int i = 0; i < currentLength; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
     return 0;
 }
